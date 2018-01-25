77d0590b6660e1d3ff8ff744adbc8abbd64c3830
unknown <mohanish9649@IND-MHP2DW74598.mastek.com>
2018-01-25 17:38:48 +0530

    2nd changes

--- src/app/postcomponet/postcomponet.component.ts
+++ src/app/postcomponet/postcomponet.component.ts
@@ -32,7 +32,10 @@ private url= 'https://jsonplaceholder.typicode.com/posts';
 
   updatePost(post)
   {
-    
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead:true}))
+    .subscribe(response=>{
+      console.log(response.json());
+    })
   }
  
 }
